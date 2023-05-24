db3f2569d94a98eacd71f09244edd0c23c695779
FranGit1 <marinicfran955@gmail.com>
2023-05-24 20:52:01 +0200

    Implementing home page

---
+++ src/components/Drink.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+interface DrinkProps {}
+
+export const Drink: React.FC<DrinkProps> = ({}) => {
+  return <></>;
+};
+export default Drink;
---
+++ src/hooks/useDrinksByCategory.ts
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Drink from "../model/Drink";
+
+function useDrinksByCategory(category: string): Drink[] {
+  const [drinks, setDrinks] = useState<Drink[]>([]);
+
+  useEffect(() => {
+    async function fetchDrinks() {
+      try {
+        const response = await axios.get(
+          `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`
+        );
+        const { drinks } = response.data;
+
+        const fetchedDrinks = drinks.map((drink: any) => ({
+          strDrink: drink.strDrink,
+          strDrinkThumb: drink.strDrinkThumb,
+          idDrink: drink.idDrink,
+        }));
+
+        setDrinks(fetchedDrinks);
+      } catch (error) {
+        console.error("Error fetching drinks:", error);
+      }
+    }
+
+    fetchDrinks();
+  }, [category]);
+
+  return drinks;
+}
+
+export default useDrinksByCategory;
