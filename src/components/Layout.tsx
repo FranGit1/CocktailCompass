09771006f1db78dbe396b327065d96bfb027bae0
FranGit1 <marinicfran955@gmail.com>
2023-05-30 23:46:52 +0200

    Finalized styling

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -47,7 +47,7 @@ export const Layout: React.FC<LayoutProps> = ({}) => {
                   Cocktail
                 </h1>
 
-                <h1 className="mb-4 text-3xl pr-10 pl-6 md:mr-5 mb-4 md:mb-0  font-extrabold text-gray-900 dark:text-white md:text-3xl lg:text-3xl">
+                <h1 className="mb-4 text-3xl pr-10 pl-6 md:mr-5 md:mb-0  font-extrabold text-gray-900 dark:text-white md:text-3xl lg:text-3xl">
                   <span className="text-transparent bg-clip-text bg-gradient-to-r to-myBlack from-darkRed">
                     Compass
                   </span>
